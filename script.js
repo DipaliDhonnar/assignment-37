2ecdcbc1a83137103b52a8c89fb37d10cc2721c5
DipaliDhonnar <drdhonnar@gmail.com>
2024-12-06 18:29:55 +0530

    script add

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-const
+const students= [
+
+    { name:'komal'},
+    { name: "maya"},
+       { name:"raj"},
+       { name:"diksha"},
+       { name:"dipali"},
+       { name:"akshada"},
+       { name: "rutuja"},
+       { name: "ram"},
+    { name:"vaishnavi"},
+    {name:"harshada"},
+
+
+];
+
+
+
+const studentsContainer = document.getElementById('students-container');
+
+function displaystudents(studentList) {
+studentsContainer.innerHTML = '';
+studentList.forEach((student) => {
+  studentsContainer.innerHTML += `
+    <div class='student-card'>
+      <span>${student.name}</span>
+    </div>
+  `;
+});
+}
+
+displaystudents(students);
+
+function filterstudents() {
+const searchQuery = document.getElementById('search').value.toLowerCase();
+const filteredstudents = students.filter(student => 
+  student.name.toLowerCase().includes(searchQuery)
+);
+displaystudents(filteredstudents);
+}
